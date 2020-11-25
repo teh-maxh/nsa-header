4fdfaafdb9cda09b5c7efe6ff56744dd930fa21b
Max Harmony <maxh@maxh.me.uk>
2020-11-25 17:42:48 -0500

    25% increase in master keywords list

--- nsa-header.php
+++ nsa-header.php
@@ -1,7 +1,7 @@
 <?php
 /**
 Plugin Name: NSA Header
-Version: 2.7
+Version: 2.71
 Description: Adds an NSA-Keywords HTTP header to reduce the effectiveness of mass spying operations.
 Author: Max Harmony
 Author URI: https://www.maxh.me.uk/
@@ -9,13 +9,14 @@ License: DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE, Version 2
 License URI: http://www.wtfpl.net/txt/copying/
 */
 
-function build_nsa_header($nKeywords = 7){
-	$keywords_master = ["amtrak", "biological", "border", "breach", "chemical", 
-	"closure", "command", "critical infrastructure", "detection", "dndo",
-	"drill", "emergency", "epidemic", "evacuation", "exercise", "facility",
-	"injection", "interstate", "landing", "maritime domain",
-	"national operations ctr", "nuclear", "ops", "radiological", "recovery",
-	"response", "riot", "security"];
+function build_nsa_header($nKeywords = 9){
+	$keywords_master = ["2600", "amtrak", "biological", "border", "breach",
+	"chemical", "closure", "command", "consulate", "critical infrastructure",
+	"detection", "disaster", "dndo", "drill", "emergency", "epidemic",
+	"evacuation", "exercise", "facility", "g7", "injection", "interstate",
+	"landing", "maritime domain", "national operations ctr", "nbic", "nuclear",
+	"ops", "plot", "radiological", "recovery", "response", "riot", "security",
+	"target"];
 	$keywords = array_rand($keywords_master, $nKeywords);
 	foreach($keywords as &$i){
 		$i = $keywords_master[$i];
